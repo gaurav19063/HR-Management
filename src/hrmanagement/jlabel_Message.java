2d4485b8e1cd918834e66b302f398de69fc4f56b
gaurav19063 <gaurav19063@iiitd.ac.in>
2019-12-30 22:56:48 +0530

    V-1.0

---
+++ src/hrmanagement/jlabel_Message.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hrmanagement;
+
+/**
+ *
+ * @author MY
+ */
+class jlabel_Message {
+
+    static void setText(String login_Successful) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/hrmanagement/mainexam.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hrmanagement;
+
+/**
+ *
+ * @author Gaurav
+ */
+ interface mainexam {
+    final int id = 0;
+    int saveResult();
+}
+
